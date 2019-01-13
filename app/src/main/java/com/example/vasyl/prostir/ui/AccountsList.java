91d915866dc679549182386c5fca717f80f14a59
Yehor <admin@MacBook-Pro-Yehor.local>
2019-01-13 11:39:59 +0200

    Delete_Account_Functionality_Added

--- app/src/main/java/com/example/vasyl/prostir/ui/AccountsList.java
+++ app/src/main/java/com/example/vasyl/prostir/ui/AccountsList.java
@@ -1,13 +1,18 @@
 package com.example.vasyl.prostir.ui;
 
 import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ListView;
 import android.widget.SimpleAdapter;
+import android.widget.Toast;
 
 import com.example.vasyl.prostir.R;
 
@@ -17,36 +22,51 @@ import java.util.HashMap;
 
 public class AccountsList extends AppCompatActivity{
 
+    private ListView listViewOfAccounts;
+    private ArrayList<HashMap<String, String>> arrayListOfAccounts;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_accounts_list);
 
         Toolbar toolbarMenu = (Toolbar) findViewById(R.id.toolbarMenu);
-        ListView listViewOfAccounts = (ListView) findViewById(R.id.listViewOfAccounts);
+        listViewOfAccounts = (ListView) findViewById(R.id.listViewOfAccounts);
 
-        ArrayList<HashMap<String, String>> arrayList = new ArrayList<>();
+        arrayListOfAccounts = new ArrayList<>();
         HashMap<String, String> map;
 
         map = new HashMap<>();
         map.put("Name", "Instagram");
         map.put("Tel", "495 501-3545");
-        arrayList.add(map);
+        arrayListOfAccounts.add(map);
 
         map = new HashMap<>();
         map.put("Name", "Telegram");
         map.put("Tel", "495 241-6845");
-        arrayList.add(map);
+        arrayListOfAccounts.add(map);
 
         map = new HashMap<>();
         map.put("Name", "Facebook");
         map.put("Tel", "495 431-5468");
-        arrayList.add(map);
+        arrayListOfAccounts.add(map);
 
-        SimpleAdapter adapter = new SimpleAdapter(this, arrayList, android.R.layout.simple_list_item_2,
+
+        listViewOfAccounts.setEnabled(false);
+        final SimpleAdapter adapter = new SimpleAdapter(this, arrayListOfAccounts, android.R.layout.simple_list_item_2,
                 new String[]{"Name", "Tel"},
                 new int[]{android.R.id.text1, android.R.id.text2});
         listViewOfAccounts.setAdapter(adapter);
+
+
+        listViewOfAccounts.setOnItemClickListener(new AdapterView.OnItemClickListener(){ // DELETE ACCOUNT
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                arrayListOfAccounts.remove(position);
+                adapter.notifyDataSetChanged();
+                listViewOfAccounts.setBackgroundColor(0);
+                listViewOfAccounts.setEnabled(false);
+            }
+        });
     }
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -70,7 +90,12 @@ public class AccountsList extends AppCompatActivity{
                 return false;
             }
             case R.id.deleteAccountMenuItem: {
-                // TODO
+                listViewOfAccounts.setEnabled(true);
+                Toast.makeText(getApplicationContext(),
+                        "Click on item to delete it",
+                        Toast.LENGTH_SHORT)
+                        .show();
+                listViewOfAccounts.setBackgroundColor(Color.parseColor("#F6F6F6"));
                 return false;
             }
             case R.id.logOutMenuItem: {
